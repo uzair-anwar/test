0f232334edc9e61f67c9023e8b0d6def84198175
uzair-anwar <uzair.anwar@devsinc.com>
2022-08-24 21:16:56 +0500

    Changed last commit

--- function.js
+++ function.js
@@ -1,19 +1,19 @@
-// // function multiply(multiplier, ...theArgs) {
-// //     return theArgs.map(x => multiplier * x);
-// //   }
-  
-// //   var arr = multiply(2, 1, 2, 3);
-// //   console.log(arr); // [2, 4, 6]
-  
-// //   let sym1 = Symbol()
-// // let sym2 = Symbol('foo')
-// // let sym3 = Symbol('foo')
-// // console.log(sym2)
+// function multiply(multiplier, ...theArgs) {
+//     return theArgs.map(x => multiplier * x);
+//   }
 
-// // const numbers = [1, 2, 3, 4, 5,6,7,8,9,10];
-// //  const newArr = numbers.map((num)=>Math.sqrt(num));
-// //  console.log(newArr)
-// // console.log(numbers.map((num)=>Math.sqrt(num)));
+//   var arr = multiply(2, 1, 2, 3);
+//   console.log(arr); // [2, 4, 6]
+
+//   let sym1 = Symbol()
+// let sym2 = Symbol('foo')
+// let sym3 = Symbol('foo')
+// console.log(sym2)
+
+// const numbers = [1, 2, 3, 4, 5,6,7,8,9,10];
+//  const newArr = numbers.map((num)=>Math.sqrt(num));
+//  console.log(newArr)
+// console.log(numbers.map((num)=>Math.sqrt(num)));
 
 // const obj= [{ key:1,value:10},
 // {key:2,value:20},
@@ -25,7 +25,6 @@
 // const map=Array.prototype.map;
 // const charCodes = map.call('Hello World',(x)=>x.charCodeAt(0));
 // console.log(charCodes)
-
 
 // let cus1={name:"uzair",email:"uzairch688@gmail.com"};
 
@@ -45,15 +44,13 @@
 // console.log(greeting1.call(cus1,['Heloo','How are you']))
 // console.log(greeting1.call(cus2,'Heloo'))
 
-
 // console.log('A');
 
 // setTimeout(() => {
 // 	console.log('B');
 // }, 0);
-	
-// console.log('C');
 
+// console.log('C');
 
 // function a(){
 //   let ab = 10;
@@ -65,5 +62,5 @@
 // //console.log(ab)
 // a();
 
-const getMax=(a,b)=>Math.max(a,b);
-console.log([1,100].reduce(getMax));
+const getMax = (a, b) => Math.max(a, b);
+console.log([1, 100].reduce(getMax));
